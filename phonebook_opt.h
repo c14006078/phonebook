a0cf36833431a5cb1ee3df6991f69a0d06ac44a8
c14006078 <c14006078@mail.ncku.edu.tw>
2016-03-05 18:03:28 +0800

    add the phonebook_detail to the data structure of phonebook
    reduce the cache references and misses

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,15 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
+#define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
+    struct phonebook_detail *pDetail;
+    struct __PHONE_BOOK_ENTRY *pNext;
+} entry;
+
+typedef struct phonebooK_detail{
+
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,8 +23,8 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
-} entry;
+
+}detail;
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
